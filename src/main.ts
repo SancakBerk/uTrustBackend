8d748843d3ef7c8b9307153bf2d786ac84b39dd4
Berk Sancak <berksancak6161@gmail.com>
2024-03-17 14:28:37 +0300

    cors origin

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://u-trust-backend-vercel.vercel.app',
+  });
   await app.listen(3000);
 }
 bootstrap();
